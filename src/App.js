63fd6588f5a6444c00db96bde3d50bac5105f6a2
Alexey Stitch <alexeyoffixial@yandex.ru>
2020-05-20 09:05:04 -0700

    Homework # 3

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Content from "./Content";
 import Footer from "./Footer";
 
 const menuItem = ['Tuda', 'Suda', 'Vperet', 'Obratno'];
-const footerItem = ['Kdsds', 'FDSDds', 'DSsdd', 'dsds'];
+const footerItem = ['Obratno', 'Vperet', 'Suda', 'Tuda'];
+
+const rybak = (fish) => {
+    console.log('Chyka ' + fish);
+}
+
+
 
 
 function App() {
   return (
     <div className="App">
- <Header menuItem={menuItem}/>
+ <Header menuItem={menuItem} leska={rybak}/>
  <Content />
- <Footer footerItem={footerItem}/>
+ <Footer footerItem={footerItem} leska={rybak}/>
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,7 @@ import FooterMenu from "./FooterMenu";
 function Footer(props) {
   return (
     <div className="footer">
+      <button onClick={() => props.leska ('Beluga')}>Улов 2</button>
       {props.footerItem.map(el => <FooterMenu key={el} massive={el}/>)}
     </div>
   );
